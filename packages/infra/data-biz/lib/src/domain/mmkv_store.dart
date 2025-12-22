74db30afd2952c5501ced908a916fa522aa25ddf
pood1e <codingpood1e@gmail.com>
2025-12-22 19:44:43 +0800

    Refactor KV store implementation and add missing methods

---
+++ packages/infra/data-biz/lib/src/domain/mmkv_store.dart
@@ -0,0 +1,58 @@
+import 'package:mmkv/mmkv.dart';
+
+import 'kv_store.dart';
+
+class MMKVStore implements KVStore {
+  final MMKV _box;
+
+  MMKVStore(MMKV box) : _box = box;
+
+  factory MMKVStore.withId(String boxId) {
+    return MMKVStore(MMKV(boxId));
+  }
+
+  @override
+  Future<void> clear() async {
+    _box.clearAll();
+  }
+
+  @override
+  Future<void> clearKey(String key) async {
+    _box.removeValue(key);
+  }
+
+  @override
+  Future<void> close() async {
+    _box.close();
+  }
+
+  @override
+  Future<int?> getInt(String key) async {
+    return _box.decodeInt(key);
+  }
+
+  @override
+  Future<String?> getString(String key) async {
+    return _box.decodeString(key);
+  }
+
+  @override
+  Future<void> saveInt(String key, int value) async {
+    _box.encodeInt(key, value);
+  }
+
+  @override
+  Future<void> saveString(String key, String value) async {
+    _box.encodeString(key, value);
+  }
+
+  @override
+  Future<bool?> getBool(String key) async {
+    return _box.decodeBool(key);
+  }
+
+  @override
+  Future<void> saveBool(String key, bool value) async {
+    _box.encodeBool(key, value);
+  }
+}
